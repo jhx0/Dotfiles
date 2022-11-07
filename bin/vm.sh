7ddc60538925f51dfb7815a1b410519a681d6e93
jhx <jhx0x00@gmail.com>
2022-11-07 17:23:36 +0100

    small fixes

--- bin/vm.sh
+++ bin/vm.sh
@@ -4,11 +4,9 @@ if [[ "$1" == "start" ]]; then
 	sudo virsh start debian-1
 	sudo virsh start freebsd-ports-1
 	sudo virsh start openbsd-1
-	sudo virsh start winsrv-1
 fi
 
 if [[ "$1" == "stop" ]]; then
-	echo "DEBUG: $PASSWORD"
 	if [[ -z "${PASSWORD}" ]]; then
 		echo "No password given, aborting"
 		exit 1
@@ -16,5 +14,5 @@ if [[ "$1" == "stop" ]]; then
 
 	sshpass -p $PASSWORD ssh v-debian-1 sudo poweroff
 	sshpass -p $PASSWORD ssh v-openbsd-1 doas halt -p
-	sshpass -p $PASSWORD ssh v-freebsd-ports-1 sudo poweroff
+	sshpass -p $PASSWORD ssh v-freebsds-1 sudo poweroff
 fi
