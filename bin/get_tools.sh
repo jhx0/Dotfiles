b1a71d2f6af04f3dc8a92397cb8ae9be3274c9d4
jhx <jhx0x00@gmail.com>
2022-11-26 16:23:19 +0100

    clone cafe

--- bin/get_tools.sh
+++ bin/get_tools.sh
@@ -7,4 +7,7 @@ echo "[INFO] Getting tools..."
 git clone https://github.com/jhx0/booky.git
 chmod +x booky/booky
 
+git clone https://github.com/rilysh/cafe.git
+chmod +x cafe/cafe
+
 echo "[OK] Done!"
