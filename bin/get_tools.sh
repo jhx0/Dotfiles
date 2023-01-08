d9f6e13b6837d05c9c16f31f4b600213a9199911
jhx <jhx0x00@gmail.com>
2023-01-08 19:06:41 +0100

    fix github repo url's

--- bin/get_tools.sh
+++ bin/get_tools.sh
@@ -10,21 +10,21 @@ chmod +x booky/booky
 git clone https://github.com/rilysh/cafe.git
 chmod +x cafe/cafe
 
-git clone git@github.com:rilysh/cpuc.git
+git clone https://github.com/rilysh/cpuc.git
 
 git clone https://github.com/rilysh/bsdfetch-sh
 chmod +x bsdfetch-sh/bsdfetch
 
-git clone git@github.com:rilysh/whatip.git
+git clone https://github.com/rilysh/whatip.git
 
 go install github.com/boyter/scc/v3@latest
 
-git clone git@github.com:cdemoulins/pamixer.git
+git clone https://github.com/cdemoulins/pamixer.git
 
 git clone git@github.com:anhsirk0/fetch-master-6000.git
 chmod +x fetch-master-6000/fm6000.pl
 
-git clone git@github.com:gentoo-btw/ramfetch.git
+git clone https://github.com/gentoo-btw/ramfetch.git
 chmod +x ramfetch/ramfetch
 
 git clone https://gitlab.com/jallbrit/cbonsai.git
