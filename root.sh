88efa23061e0a3658c727dbc5bb60bb50926bbde
jhx <jhx0x00@gmail.com>
2024-06-16 14:29:51 +0200

    check for sudo

--- root.sh
+++ root.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-cp .bash* /root/
-cp -r .vim /root/
-cp .nanorc /root/
-cp .vimrc /root/
-mkdir -p /root/bin
-cp bin/pfetch /root/bin/
+which sudo > /dev/null 2>&1
+if [ "$?" -eq 1 ]; then
+	echo "[ERROR] sudo not available!"
+	exit 1
+fi
+
+sudo cp .bash* /root/
+sudo cp -r .vim /root/
+sudo cp .nanorc /root/
+sudo cp .vimrc /root/
+sudo mkdir -p /root/bin
+sudo cp bin/pfetch /root/bin/
