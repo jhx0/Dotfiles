c4f06afd662f3fe20a27e167f7fe47a1c5bde598
jhx <jhx0x00@gmail.com>
2025-07-02 16:47:08 +0200

    added: mpv, conky, fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set -U fish_greeting
+set -U fish_prompt_pwd_dir_length 0
+
+function fish_prompt
+    set_color -o FF5549; echo -n [; set_color normal
+	set_color -o 2AFF26; echo -n (hostname); echo -n " "; set_color normal
+	set_color -o 55FFF6; echo -n (prompt_pwd); set_color normal
+    set_color -o FF5549; echo -n ]; echo -n " "; set_color normal
+	set_color -o FF5549; echo -n '$ '; set_color normal
+end
